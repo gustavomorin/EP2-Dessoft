ea1d3b729831b6e5f2ebf5aa0d0facee1c72c9b0
Gustavo Morin de Sousa <gustavoms9@al.insper.edu.br>
2022-05-05 14:42:29 -0300

    Apresentacao

---
+++ jogo.py
@@ -0,0 +1,12 @@
+print(' ============================ ')
+print('|                            |')
+print('| BEM VINDO AO INSPER PAÍSES |')
+print('|                            |')
+print(' ==== DESIGN DE SOFTWARE ==== ')
+
+print('\nComandos:')
+
+print('\ndica          - entra no mercado de dicas')
+print('desisto       - desiste da rodada')
+print('inventario    - exibe sua posição')
+
