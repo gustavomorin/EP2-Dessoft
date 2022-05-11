436ca58529a59e985495962db1305f8afbb4c9ca
Joao Ferraz <joaoaf@al.insper.edu.br>
2022-05-11 14:41:37 -0300

    Merged

--- jogo3.py
+++ jogo3.py
@@ -20,10 +20,7 @@ while palpite.lower() != 'desisto':
     dados_paises = normaliza(dados)
     sorteado = sorteia_pais(dados_paises)
     print(sorteado)
-<<<<<<< HEAD
-=======
     lista = []
->>>>>>> 0f917b8a6588613dd636cbf25043e2c2c555c34f
 
     for tentativa in range(20,0, -1):
         print('Um país foi escolhido, tente advinhar!')
@@ -38,13 +35,6 @@ while palpite.lower() != 'desisto':
                 longitudeA = dados_paises[palpite.lower()]['geo']['longitude']
                 latitudeB = dados_paises[sorteado]['geo']['latitude']
                 longitudeB = dados_paises[sorteado]['geo']['longitude']
-<<<<<<< HEAD
-        lista = []    
-        distancias = haversine(r, latitudeA, longitudeA, latitudeB, longitudeB)
-        print('\nDistância:\n{0} -> {1}'.format(palpite, int(distancias)))
-        lista.append([palpite,int(distancias)])
-        
-=======
         distancias = haversine(r, latitudeA, longitudeA, latitudeB, longitudeB)
         ordem = adiciona_em_ordem(palpite, distancias, lista)
         lista.append([palpite, distancias])
@@ -52,7 +42,6 @@ while palpite.lower() != 'desisto':
 
         for termo in ordem:
             print('{0} -> {1} km'.format(termo[0], int(termo[1])))
->>>>>>> 0f917b8a6588613dd636cbf25043e2c2c555c34f
 
         if palpite == 'desisto' or palpite == 'DESISTO':
             print('Que deselegante desistir, o pais era: {0}'.format(sorteado))
