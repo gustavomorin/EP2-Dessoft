91461d7e7eefefe7e5fb5cc3273d8d92a27528bd
Joao Ferraz <joaoaf@al.insper.edu.br>
2022-05-11 15:27:13 -0300

    jogo com lopping da dica

--- jogo4.py
+++ jogo4.py
@@ -57,8 +57,40 @@ while palpite.lower() != 'desisto':
 
         for termo in lista:
                 print('{0} -> {1} km'.format(termo[0], int(termo[1])))
-    
-        if palpite not in dados_paises.keys():
+        print('\nDicas:')
+        if palpite == 'dica':
+            print('----------------------------------------')
+            print('1. Cor da Bandeira  - custa 4 tentativas')
+            print('2. Letra da capital - custa 3 tentativas')
+            print('3. Área             - custa 6 tentativas')
+            print('4. População        - custa 5 tentativas')
+            print('5. Continente       - custa 7 tentativas')
+            print('0. Sem dica                             ')
+            print('----------------------------------------')
+            dica = ''
+            while dica != '0' and dica != '1' and dica != '2' and dica != '3' and dica != '4' and dica != '5' :
+                dica = input('Escolha a sua opção [0|1|2|3|4|5]:')
+                if dica == '0' and dica == '1' and dica == '2' and dica == '3' and dica == '4' and dica == '5' :
+                    break
+                print('Opção inválida')
+            if dica == '0':
+                print('sem dica')
+                tentativa += 2
+            elif dica == '1':
+                print(2)
+                tentativa -=4
+            elif dica == '2':
+                tentativa -= 3
+            elif dica == '3':
+                tentativa -= 6
+            elif dica == '4':
+                tentativa -= 5
+            elif dica == '5':
+                tentativa -= 7
+
+
+
+        elif palpite not in dados_paises.keys():
             print('\nPaís desconhecido')
     
     jogar_novamente = input('Jogar novamente? [s/n]')
