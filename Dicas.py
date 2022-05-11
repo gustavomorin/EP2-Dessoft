f184b5a15c06bece4810a50fd858b785d0e86acf
Joao Ferraz <joaoaf@al.insper.edu.br>
2022-05-11 14:27:48 -0300

    desenho dicas

---
+++ Dicas.py
@@ -0,0 +1,8 @@
+print('----------------------------------------')
+print('1. Cor da Bandeira  - custa 4 tentativas')
+print('2. Letra da capital - custa 3 tentativas')
+print('3. Área             - custa 6 tentativas')
+print('4. População        - custa 5 tentativas')
+print('5. Continente       - custa 7 tentativas')
+print('0. Sem dica                             ')
+
