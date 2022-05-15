9d19516fa6238001cbfa6db92199e0260a385df1
Joao Ferraz <joaoaf@al.insper.edu.br>
2022-05-15 16:10:19 -0300

    Prints arrumados

--- jogo4.py
+++ jogo4.py
@@ -24,7 +24,7 @@ print(' ==== DESIGN DE SOFTWARE ==== ')
 
 print('\nComandos:')
 
-print('\ndica          - entra no mercado de dicas')
+print('\ndica              - entra no mercado de dicas')
 print('desisto       - desiste da rodada')
 print('inventario    - exibe sua posição')
 
